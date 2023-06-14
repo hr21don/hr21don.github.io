75c331fbb8e4df1d7a4671316200aa94f9649a08
Helitha Rupasinghe <hr21don>
2023-06-14 08:00:22 +0100

    updated index.html && main.js

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// Credit: Pavel Dobryakov (https://codepen.io/PavelDoGreat/pen/zdWzEL)
 'use strict';
 
 const canvas = document.getElementsByTagName('canvas')[0];
@@ -11,7 +12,7 @@ let config = {
     PRESSURE_DISSIPATION: 0.8,
     PRESSURE_ITERATIONS: 25,
     CURL: 30,
-    SPLAT_RADIUS: 0.005
+    SPLAT_RADIUS: 0.004 
 }
 
 let pointers = [];
@@ -625,9 +626,51 @@ canvas.addEventListener('mousemove', (e) => {
     pointers[0].dy = (e.offsetY - pointers[0].y) * 10.0;
     pointers[0].x = e.offsetX;
     pointers[0].y = e.offsetY;
-    
+});
+
+canvas.addEventListener('touchmove', (e) => {
+    e.preventDefault();
+    const touches = e.targetTouches;
+    for (let i = 0; i < touches.length; i++) {
+        let pointer = pointers[i];
+        pointer.moved = pointer.down;
+        pointer.dx = (touches[i].pageX - pointer.x) * 10.0;
+        pointer.dy = (touches[i].pageY - pointer.y) * 10.0;
+        pointer.x = touches[i].pageX;
+        pointer.y = touches[i].pageY;
+    }
+}, false);
+
+canvas.addEventListener('mousedown', () => {
     pointers[0].down = true;
     pointers[0].color = [Math.random() + 0.2, Math.random() + 0.2, Math.random() + 0.2];
+});
+
+canvas.addEventListener('touchstart', (e) => {
+    e.preventDefault();
+    const touches = e.targetTouches;
+    for (let i = 0; i < touches.length; i++) {
+        if (i >= pointers.length)
+            pointers.push(new pointerPrototype());
+
+        pointers[i].id = touches[i].identifier;
+        pointers[i].down = true;
+        pointers[i].x = touches[i].pageX;
+        pointers[i].y = touches[i].pageY;
+        pointers[i].color = [Math.random() + 0.2, Math.random() + 0.2, Math.random() + 0.2];
+    }
+});
+
+window.addEventListener('mouseup', () => {
+    pointers[0].down = false;
+});
+
+window.addEventListener('touchend', (e) => {
+    const touches = e.changedTouches;
+    for (let i = 0; i < touches.length; i++)
+        for (let j = 0; j < pointers.length; j++)
+            if (touches[i].identifier == pointers[j].id)
+                pointers[j].down = false;
 });
 
 document.addEventListener('DOMContentLoaded',function(event){let typed = new Typed(".typeline", {
